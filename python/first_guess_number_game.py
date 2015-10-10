959391fbd4bb4bd3fe7eaaaec2d2769229879fe2
lihuating <lihuating0722@163.com>
2015-10-10 23:41:36 +0800

    add python first game

---
+++ python/first_guess_number_game.py
@@ -0,0 +1,12 @@
+print("-------------------我爱小鱼工作室--------------")
+temp = input("guess which number in my mind:")
+#input 
+guess = int(temp)
+if guess ==8:
+    print("wo cao ,ni shi wo xin li de hui chong ma")
+    print("cai zhong shi mei you jiang li de ")
+else:
+    print("jiu zhi dao ni cai bu zhong")
+    print(" the number in my mind is 8888888")
+print(" Game Over")
+print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
